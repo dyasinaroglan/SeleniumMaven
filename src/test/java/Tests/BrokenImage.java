d66c4efd7e9e7df76150524dd3383a7e7131e6e4
YASİN AROĞLAN <yasinargln264@gmail.com>
2022-02-20 22:52:10 +0300

    20.02.22

--- src/test/java/Tests/BrokenImage.java
+++ src/test/java/Tests/BrokenImage.java
@@ -1,22 +1,18 @@
 package Tests;
 
-import org.apache.http.*;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.params.HttpParams;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.IOException;
-import java.util.Locale;
 
-public class BrokenLinks {
-
-
+public class BrokenImage {
     public static void main(String[] args) throws IOException {
         System.setProperty("webdriver.chrome.driver", "C:\\SELENIUM\\chromeDriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -29,30 +25,13 @@ public class BrokenLinks {
         //img--->hepsinde src attribute vardır.
         System.out.println(brokenImgUrl);
 
-
         HttpClient client = HttpClientBuilder.create().build();
-        HttpGet request = new HttpGet("http://the-internet.herokuapp.com/status_codes/500");
+        HttpGet request = new HttpGet(brokenImgUrl);
         HttpResponse response = client.execute(request);
-
         int statusCode = response.getStatusLine().getStatusCode();
         System.out.println(statusCode);
 
 
 
-
-        //status code 200 dönmesi başarılı olduğu anlamına gelir
-        //status cıde 500 olsaydı hatalı olduğunu linkin bozuk olduğunu anlamamız gerekecekti.
-
-        /*
-        200 – Geçerli Bağlantı
-        404 – Bağlantı bulunamadı
-        400 – Kötü istek
-        401 – Yetkisiz
-        500 – Dahili Hata
-         */
-
-
-        }
-
     }
-
+}
